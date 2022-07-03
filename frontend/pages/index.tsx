6a5175ad935fbcec01713e999679aa324bf0dba9
meesh <meesh@found.com>
2022-07-02 17:40:37 -0700

    css wip

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -17,64 +17,27 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to{' '}
-          <a href="https://nextjs.org">
-            Next.js!
-          </a>
+          Take The Burns Depression Checklist
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>
-            pages/index.tsx
-          </code>
+          From{' '}
+          <a href="https://www.amazon.com/Feeling-Good-New-Mood-Therapy-ebook/dp/B009UW5X4C">
+            Feeling Good: The New Mood Therapy, by
+            David D. Burns
+          </a>
         </p>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs"
-            className={styles.card}
-          >
-            <h2>Documentation &rarr;</h2>
-            <p>
-              Find in-depth information about
-              Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className={styles.card}
-          >
-            <h2>Learn &rarr;</h2>
-            <p>
-              Learn about Next.js in an
-              interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>
-              Discover and deploy boilerplate
-              example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site
-              to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <table className="table-auto">
+          <tbody>
+            <tr>
+              <td>
+                Feeling sad or down in the dumps
+              </td>
+              <td>TODO</td>
+            </tr>
+          </tbody>
+        </table>
       </main>
     </div>
   );
