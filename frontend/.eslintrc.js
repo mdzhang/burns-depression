dcc71c58e3349a0f5c9bdbefc7821d1fc12060cc
meesh <meesh@found.com>
2022-07-25 08:21:58 -0700

    Add button range

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -36,5 +36,10 @@ module.exports = {
     // OOTB not working w/ react app
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
+    // no-unused vars will misidentify TS types
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+    ],
   },
 };
