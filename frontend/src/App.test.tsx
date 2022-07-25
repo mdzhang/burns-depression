bdcc2555cd4eabe7119854839338020297b8116e
meesh <meesh@found.com>
2022-07-24 17:50:05 -0700

    self review

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getAllByText(/Burns Depression Checklist/i);
+  expect(titleElement.length).toBeGreaterThan(0);
 });
