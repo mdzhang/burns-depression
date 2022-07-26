50c9d5167f0339b52481e9ddb409682de59867a9
meesh <meesh@found.com>
2022-07-25 17:20:13 -0700

    show level of depression

--- frontend/src/pages/desktop/Quiz.tsx
+++ frontend/src/pages/desktop/Quiz.tsx
@@ -3,18 +3,28 @@ import Formsy from 'formsy-react';
 
 import ButtonRange from '../../components/desktop/forms/ButtonRange';
 import data from '../../data/questions.json';
+import ranges from '../../data/ranges.json';
 
 import styles from './Quiz.module.css';
 
 function Quiz() {
-  const [points, setPoints] = useState(0);
+  const [levelOfDepression, setLevelOfDepression] = useState('');
+  const [showResults, setShowResults] = useState(false);
 
   const updatePoints = (model: { [key: string]: string; }) => {
     const total = Object.values(model)
       .map((val) => (val === '' ? 0 : parseInt(val, 10)))
       .reduce((a, b) => a + b, 0);
 
-    setPoints(total);
+    setShowResults(true);
+
+    const range = ranges.find((r) => total >= r.min && total <= r.max);
+    if (range) {
+      setLevelOfDepression(range.result);
+    } else {
+      // eslint-disable-next-line no-console
+      console.warn(`Could not find matching range for total: ${total}`);
+    }
 
     const date = new Date(Date.now()).toISOString().slice(0, 10);
     const result = {
@@ -32,7 +42,7 @@ function Quiz() {
         Take The Burns Depression Checklist
       </h1>
 
-      <p className={styles.description}>
+      <p>
         From
         {' '}
         <a href="https://www.amazon.com/Feeling-Good-New-Mood-Therapy-ebook/dp/B009UW5X4C">
@@ -41,44 +51,40 @@ function Quiz() {
         </a>
       </p>
 
-      <Formsy onValidSubmit={updatePoints} onChange={updatePoints}>
+      <Formsy onValidSubmit={updatePoints}>
         <table className="table-auto rounded-lg">
           <tbody>
-            {data
-              .map((c) => c.questions)
-              .flat()
-              .map((question) => (
-                <tr key={question}>
-                  <td
-                    className="border px-4 py-2"
-                    key={question}
-                  >
-                    {question}
+            {data.map((entry) => (
+              <>
+                <tr>
+                  <td className="px-4 py-2 pt-8">
+                    <b>
+                      {entry.category}
+                    </b>
                   </td>
-                  <td
-                    className="border px-4 py-2"
-                  >
-                    <ButtonRange name={question} value="" />
-                  </td>
+                  <td />
                 </tr>
-              ))}
-
-            <tr>
-              <td
-                className="border px-4 py-2"
-              />
-              <td
-                className="border px-4 py-2"
-              >
-                Total:
-                {' '}
-                <b>{points}</b>
-              </td>
-            </tr>
+                {entry.questions.map((question) => (
+                  <tr key={question}>
+                    <td
+                      className="border px-4 py-2"
+                      key={question}
+                    >
+                      {question}
+                    </td>
+                    <td
+                      className="border px-4 py-2"
+                    >
+                      <ButtonRange name={question} value="" />
+                    </td>
+                  </tr>
+                ))}
+              </>
+            ))}
           </tbody>
         </table>
 
-        <div className="my-4 grid place-items-center">
+        <div className="my-4 grid place-items-center pt-4">
           <button
             type="submit"
             className="flex-shrink-0  text-sm border-4 text-white py-1 px-2 rounded bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700"
@@ -86,7 +92,18 @@ function Quiz() {
             See my results
           </button>
         </div>
+
       </Formsy>
+
+      {showResults && (
+        <div className="text-xl">
+          You have
+          {' '}
+          <span className="font-bold">
+            { levelOfDepression.toLowerCase() }
+          </span>
+        </div>
+      )}
     </main>
   );
 }
