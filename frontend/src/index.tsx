3cf103970698bdcd5c3ff1db59ac9c80bc401971
meesh <meesh@found.com>
2022-07-26 13:31:22 -0700

    Add sentry for err reporting
    - https://vercel.com/integrations/sentry

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: 'https://27266edfc06c441caed8e8fd123ac5d3@o206889.ingest.sentry.io/6603775',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
