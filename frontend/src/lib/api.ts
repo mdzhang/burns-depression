629c7427043733b07379bc9c019113c9c12da720
meesh <meesh@found.com>
2022-07-27 08:38:05 -0700

    Add initial supabase integration (#26)

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+import { REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY } from './constants';
+
+export const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY);
+
+export default supabase;
