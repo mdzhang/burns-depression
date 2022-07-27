0239d7d113a9c96b36458da9809c40a22c399637
meesh <meesh@found.com>
2022-07-27 09:16:46 -0700

    move to component

---
+++ frontend/src/components/Topbar.jsx
@@ -0,0 +1,68 @@
+import { useSearchParams } from 'react-router-dom';
+
+function Topbar() {
+  const [searchParams] = useSearchParams();
+  const enableLogin = searchParams.get('enableLogin') === '1';
+
+  return (
+    <header className="bg-white">
+      <div
+        className="flex items-center h-16 max-w-screen-xl gap-8 px-4 mx-auto sm:px-6 lg:px-8"
+      >
+        <a className="block text-teal-600" href="/">
+          <span className="sr-only">Home</span>
+        </a>
+
+        <div className="flex items-center justify-end flex-1 md:justify-between">
+          <nav className="hidden md:block" aria-labelledby="header-navigation">
+            <h2 className="sr-only" id="header-navigation">Header navigation</h2>
+
+            <ul className="flex items-center gap-6 text-sm">
+              <li>
+                <a className="text-gray-500 transition hover:text-gray-500/75" href="/take-quiz">
+                  Take Quiz
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:gap-4 sm:flex">
+              {enableLogin && (
+                <a
+                  className="hidden sm:block px-5 py-2.5 text-sm font-medium text-teal-600 bg-gray-100 rounded-md hover:text-teal-600/75 transition"
+                  href="/login"
+                >
+                  Login
+                </a>
+              )}
+            </div>
+
+            <button
+              type="button"
+              className="block p-2.5 text-gray-600 transition bg-gray-100 rounded md:hidden hover:text-gray-600/75"
+            >
+              <span className="sr-only">Toggle menu</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Topbar;
