a6d1049bfd4fa400b8ada7480bc862dba509f357
meesh <meesh@found.com>
2022-07-29 16:30:43 -0700

    login modal + user greeting

--- frontend/src/components/RightMenu.tsx
+++ frontend/src/components/RightMenu.tsx
@@ -1,12 +1,18 @@
 import { Menu } from 'antd';
+import { useState } from 'react';
 import { User } from '../lib/types';
 import { supabase } from '../lib/api';
+import LoginModal from './LoginModal';
 
 interface Props {
   user: User | null;
 }
 
 function RightMenu({ user }: Props) {
+  console.log(user);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const closeModal = () => setIsModalVisible(false);
+
   const onLogout = async () => {
     const { error } = await supabase.auth.signOut();
     if (error) {
@@ -14,18 +20,25 @@ function RightMenu({ user }: Props) {
       console.warn(`Error logging out: ${error}`);
     }
   };
-  // const username = user?.user_metadata?.full_name;
 
   return (
-    <Menu mode="horizontal">
-      <Menu.Item key="login-logout">
-        {user ? (
-          <a href="/logout" onClick={onLogout}>Logout</a>
-        ) : (
-          <a href="/login">Login</a>
-        )}
-      </Menu.Item>
-    </Menu>
+    <>
+      <Menu mode="horizontal">
+        <div>
+          Hi,
+          {' '}
+          {user?.user_metadata?.full_name}
+        </div>
+        <Menu.Item key="login-logout">
+          {user ? (
+            <a href="/logout" onClick={onLogout}>Logout</a>
+          ) : (
+            <a href="#" onClick={() => setIsModalVisible(true)}>Login</a>
+          )}
+        </Menu.Item>
+      </Menu>
+      <LoginModal visible={isModalVisible} onClose={closeModal} />
+    </>
   );
 }
 
