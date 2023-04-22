0b78cd988eb1001c2da573810f5fc36074dbcd32
meesh 🤘 <meesh@found.com>
2023-04-22 11:39:34 -0500

    add frog info

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -4,3 +4,19 @@ export const {
 } = process.env;
 
 export const TOPBAR_HEIGHT = 54;
+
+export const ANSWER_LEGEND = {
+  0: 'Not at all',
+  1: 'Somewhat',
+  2: 'Moderately',
+  3: 'A lot',
+  4: 'Extremely',
+};
+
+export const FROG_COLORS = {
+  0: '#FF6E65',
+  1: '#75C9FF',
+  3: '#FFE482',
+  4: '#FCAC4D',
+  5: '#FF6E65',
+};
