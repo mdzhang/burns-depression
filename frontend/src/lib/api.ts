e0bf81f97f7ea548d672e7acb7948e45805869bd
meesh 🤘 <meesh@found.com>
2023-04-22 14:01:09 -0500

    switch to vite

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY } from '@burns-depression/constants';
+import { REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY } from '@burns-depression/lib/constants';
 
 export const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_KEY);
 
