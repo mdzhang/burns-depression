dcf3d07b470eeb871daa1ef770c1bbba03b00a54
meesh <zhang.michelle.d@gmail.com>
2023-04-22 11:28:36 -0500

    Fix scroll (#40)

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,1 +1,6 @@
-export const { REACT_APP_SUPABASE_URL = 'unknown', REACT_APP_SUPABASE_KEY = 'unknown' } = process.env;
+export const {
+  REACT_APP_SUPABASE_URL = 'unknown',
+  REACT_APP_SUPABASE_KEY = 'unknown',
+} = process.env;
+
+export const TOPBAR_HEIGHT = 54;
