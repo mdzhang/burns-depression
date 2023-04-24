f80f02795452f01c631e65ad12a340dded988c23
meesh 🤘 <meesh@found.com>
2023-04-24 08:32:16 -0500

    safe area fix

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,22 @@
-import { WebView } from "react-native-webview";
-import Constants from "expo-constants";
-import { REACT_APP_BASE_URL } from "@env"
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import { REACT_APP_BASE_URL } from '@env'
 
 const uri = `https://${REACT_APP_BASE_URL }`;
 console.log(`URI is ${uri}`);
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
 export default function App() {
   return (
-    <WebView
-      source={{ uri }}
-      style={{ marginTop: 24 }}
-    />
+    <SafeAreaView style={styles.container}>
+      <WebView source={{ uri }} />
+    </SafeAreaView>
   );
 }
